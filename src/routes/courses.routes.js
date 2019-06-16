e62fcc3a7a61a7533ed9297b502ad913dee35c6a
David Yepes <djvash01@gmail.com>
2019-06-15 20:30:10 -0500

    Agregando navegacion y footer

--- src/routes/courses.routes.js
+++ src/routes/courses.routes.js
@@ -7,6 +7,11 @@ router.get('/applicant/', async (req, res)=>{
     res.render('courses/applicant',{courses});
 });
 
+router.get('/enrolles/', async (req,res)=>{
+    const courses = await Course.find({status:'Disponible'});
+    res.render('courses/enrolles',{courses});
+});
+
 router.get('/', async (req, res)=>{
     const courses = await Course.find().sort({created_at:'desc'});
     res.render('courses/courses',{courses});
@@ -37,19 +42,25 @@ router.post('/', async (req, res)=>{
     
 });
 
-router.put('/enroll/:id', async (req, res) =>{
-    const {dni, name, email, phone} = req.body;
+router.put('/enroll/', async (req, res) =>{
+    const {id, dni, name, email, phone} = req.body;
+    const answer=[];
+    const courses = await Course.find({status:'Disponible'});
     if(!(dni && name && email && phone)){
-        res.json({status:'failed', description:'Los datos ingresados no son validos'});
+        answer.push({status:'danger', description:'Los datos ingresados no son validos'})
+        res.render('courses/applicant',{courses,answer});
     }else{
-        const course = await Course.findOne({id:req.params.id});
+        const course = await Course.findById(id);
         const enrolled = course.enrollees.find(enrolled=>enrolled.dni===dni);
         if(enrolled){
-            res.json({status:'failed', description:'Usted ya se encuentra inscrito en este curso'});
+            answer.push({status:'danger', description:'Usted ya se encuentra inscrito en este curso'})
+            res.render('courses/applicant',{courses, answer});
         }else{
-            course.enrollees.push(req.body);
-            await Course.findByIdAndUpdate(course._id,course);
-            res.json({status:'enrrollment succes'});
+            const applicant = {dni,name,email,phone};
+            course.enrollees.push(applicant);
+            await Course.findByIdAndUpdate(id,course);
+            answer.push({status:'success', description:'Registro exitoso'})
+            res.render('courses/applicant',{courses, answer});
         }
         
     }
@@ -69,16 +80,20 @@ router.put('/:id', async (req,res)=>{
 
 router.put('/delete/:id', async (req,res)=>{
     const {dni} =req.body;
+    const answer=[];
     const course = await Course.findOne({id:req.params.id});
     const enrolled = course.enrollees.find(enrolled=>enrolled.dni===dni);
     let index = course.enrollees.indexOf(enrolled);
     if(index!==-1){
         course.enrollees.splice(index,1);
         await Course.findByIdAndUpdate(course._id,course);
-        res.json({status:'Enrolled Deleted'});
+        const courses = await Course.find({status:'Disponible'});
+        answer.push({status:'warning', description:'Aspirante eliminado'})
+        res.render('courses/enrolles',{courses, answer});
     }else{
-        res.json({status:'failed', description:'No se encontro estudiante con ese dni'});
-
+        const courses = await Course.find({status:'Disponible'});
+        answer.push({status:'danger', description:'No se encontro estudiante con ese dni'})
+        res.render('courses/enrolles',{courses, answer});
     }
     
 });
