869c4569e8f6a1ae0a238ecd0902301fe0799499
David Yepes <djvash01@gmail.com>
2019-07-16 21:33:56 -0500

    Se agrega la funcionalidad de envio de correo a los registrados

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const passport = require('passport');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 
 router.get('/register', (req, res) =>{
@@ -25,9 +27,16 @@ router.post('/register', async (req, res)=>{
         res.render('users/register',{answer, dni, name, email, phone, password,confirm_password});
     }else{
         try{
+            const msg = {
+                to: email,
+                from: 'djvash01@gmail.com',
+                subject: 'Bienvenido a APPTDEA',
+                text: 'Eres bienvenido a APPTDEA donde puedes encontrar infinidad de cursos'
+              }; 
             const user = new User({dni, name, email, phone, password, rol});
             user.password = await user.encryptPassword(password);    
             await user.save();
+            sgMail.send(msg);
             req.flash('answer',{status: 'success', description: 'Has sido registrado con exito'});
             res.redirect('/api/users/login');
         }catch(err){
